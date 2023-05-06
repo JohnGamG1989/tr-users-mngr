e3775620da6377d45d45b7d4b11efbb8b51c19ab
Andres Santacruz <31628943+ansantacruz@users.noreply.github.com>
2023-05-06 12:25:30 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
-
 export default {
     PORT: 8080,
     apiPath: '',
-    databaseHost: '35.192.7.203',
+    databaseHost: '34.121.184.65',
     databasePass: 'Pr0s3gu1r.2709*',
     databasePort: '3306',
     databaseUser: 'root',
@@ -11,13 +10,4 @@ export default {
     databesePoolMax:  '7',
     databesePoolMin: '0',
     logSentenceSQL: true,
-    // activeEvent: process.env.ACTIVE_EVENT || '',
-    // apiEvent: process.env.API_HC_PENTAGONO || '',
-    // apikeyTvs: process.env.TVS_API_KEY || '',
-    // apikeyHc: process.env.API_KEY_HC || '',
-    // apiTvsSendgrid: process.env.API_TVS_SENDGRID || '',
-    // environment: process.env.NODE_ENV || 'qa',
-    // fetchTimeout: process.env.FETCH_TIMEOUT || 29000
-    // PORT: process.env.PORT || '9087',
-
 };
